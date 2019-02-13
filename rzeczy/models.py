042aa8d296c2871efd1528157c4cef2959a23da6
OleksVovk <oleks.vovk@gmail.com>
2019-02-13 22:40:25 +0100

    models updated

--- rzeczy/models.py
+++ rzeczy/models.py
@@ -2,10 +2,13 @@ from django.db import models
 
 # Create your models here.
 
+# jeden do wielu Organization --> Present
+
 
 class Organization(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField()
+    address = models.CharField(max_length=64)
 
     def __str__(self):
         return str(self.name), str(self.description)
@@ -14,6 +17,7 @@ class Organization(models.Model):
 class Present(models.Model):
     name = models.CharField(max_length=64)
     description = models.TextField()
+    date = models.DateField()
 
     def __str__(self):
         return str(self.name), str(self.description)
